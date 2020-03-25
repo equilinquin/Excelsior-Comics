038579a0b2e3f65775108c9057ace79b76a8ffa7
jaredbucko <jaredbucko@icloud.com>
2020-03-24 20:16:00 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>This is our awesome app!</h1>
+      <h1>By Katie, Liz, Luke, and Jared</h1>
+    </div>
+  );
+}
+
+export default App;
