659789670486881bbcbf03091c81bb5d8a5b89c8
cavner <katieavner@gmail.com>
2020-04-04 13:08:05 -0700

    updated div tag

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,33 +2,50 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="row">
-      <div className="col s3" id="login-left">
-        <div className="login-form">
-          <form className="login">
-            <div className="form-group">
-              <label for="exampleInputEmail1"></label>
-              <input type="email" class="form-control" id="email-input" placeholder="Email" />
-            </div>
-            <div className="form-group">
-              <label for="exampleInputPassword1"></label>
-              <input type="password" class="form-control" id="password-input" placeholder="Password" />
-            </div>
-            <div>
-              <button type="submit" class="btn btn-default">Login</button>
-              <a href="signup.html"></a><button class="btn btn-default">Sign Up</button>
-            </div>
-          </form>
+    <div>
+      <div className="row">
+        <div className="col s3" id="login-left">
+          <div className="login-form">
+            <form className="login">
+              <div className="form-group">
+                <label for="exampleInputEmail1"></label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email-input"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <label for="exampleInputPassword1"></label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password-input"
+                  placeholder="Password"
+                />
+              </div>
+              <div>
+                <button type="submit" class="btn btn-default">
+                  Login
+                </button>
+                <a href="signup.html"></a>
+                <button class="btn btn-default">Sign Up</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="col s9 image-right">
-      <div className="text">
-        <h1>Welcome to Excelsior Comics!</h1>
-        <div className="P"> Discover the world of digital comics.
-                    <br />
-                      Happy Hunting!
-            </div>
+      <div className="col s9 image-right">
+        <div className="text">
+          <h1>Welcome to Excelsior Comics!</h1>
+          <div className="P">
+            {" "}
+            Discover the world of digital comics.
+            <br />
+            Happy Hunting!
+          </div>
+        </div>
       </div>
     </div>
   );
