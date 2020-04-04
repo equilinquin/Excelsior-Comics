3bdf575fac5213f49b59c47a98b64fec0d8f360a
cavner <katieavner@gmail.com>
2020-04-04 12:52:36 -0700

    starter for Signup, Login and Home.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,8 +2,34 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <h1>Have an account? log in here!</h1>
+    <div className="row">
+      <div className="col s3" id="login-left">
+        <div className="login-form">
+          <form className="login">
+            <div className="form-group">
+              <label for="exampleInputEmail1"></label>
+              <input type="email" class="form-control" id="email-input" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1"></label>
+              <input type="password" class="form-control" id="password-input" placeholder="Password" />
+            </div>
+            <div>
+              <button type="submit" class="btn btn-default">Login</button>
+              <a href="signup.html"></a><button class="btn btn-default">Sign Up</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div className="col s9 image-right">
+      <div className="text">
+        <h1>Welcome to Excelsior Comics!</h1>
+        <div className="P"> Discover the world of digital comics.
+                    <br />
+                      Happy Hunting!
+            </div>
+      </div>
     </div>
   );
 }
