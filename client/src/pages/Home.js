b9d71815dfc8dd7a86f62a6ffd952837395b33ad
jaredbucko <jaredbucko@icloud.com>
2020-04-07 21:05:01 -0700

    updated homepage layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import marvel from "../utils/marvel-api";
 import Search from "../components/Search/index";
 import ComicCards from "../components/ComicCards/index";
@@ -7,7 +8,6 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      comics: [{}],
       order: "ascending",
       sortedComics: [{}],
       handleSearch: (e) => {
@@ -25,48 +25,58 @@ export default class Home extends Component {
   // Uncomment once API is called
    componentDidMount() {
      // waits until component is called on to show up in broswer and then does a function
-     marvel.getComics("Avengers", function(result) {
-       console.log(result);
+     marvel.getComics("Avengers", function(APIresults) {
+       console.log(APIresults);
      });
     //  .then((APIresults) => {
     //    // as soon as component is ready on users browsers, results will be waiting to do something with them
     //    this.setState({
-    //      users: APIresults.data.results,
+    //      comics: APIresults.data.results,
     //      //.data comes from API
-    //      filteredSearh: APIresults.data.results,
+    //      filteredSearch: APIresults.data.results,
     //      //build table and get results to display
     //      //this.state.users to show up on page
     //      //map function used when info inside db will be changing and it will need to be updated and displayed
     //    });
-    //    console.log(this.state.filteredSearch);
+    //    console.log(this.state.comics);
     //    //sort by one category and filter by one property
     //  });
    }
   render() {
     return (
-      <div className="container">
-        <div className="wrapper">
-          <Search handleSearch={this.state.handleSearch} />
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 col-md-offset-3">
-                <h2>
-                  Welcome <span className="member-name"></span>
-                </h2>
-              </div>
+      <div>
+
+        <nav>
+          <div className="nav-wrapper">
+            <Link to="/home" className="brand-logo">Welcome</Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link className="navbar-brand" to="/login">Logout</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+
+          <div className="row">
+            <div className="col s12">
+              <h5 className="center-align">Start searching by character or title:</h5>
             </div>
           </div>
-          <ComicCards sortedComics={this.state.sortedComics} />
-          <nav className="navbar navbar-default">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="/login">
-                  Logout
-                </a>
-              </div>
+
+          <div className="container row">
+            <div className="col s12">
+              <Search handleSearch={this.state.handleSearch} />
             </div>
-          </nav>
+          </div>
+
+          <div className="row">
+            <div className="col s12">
+              <ComicCards sortedComics={this.state.sortedComics} />
+            </div>
+          </div>
+
         </div>
+
       </div>
     );
   }
