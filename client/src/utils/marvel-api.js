518fc511f04dfa0a9a9c3afd494e1daf83be85bb
luke-robinett <lukebootcamp@gmail.com>
2020-04-07 17:21:13 -0700

    Added utils/marvel-api.js helper script

---
+++ client/src/utils/marvel-api.js
@@ -0,0 +1,27 @@
+// Client side interface for Node server comic search API
+
+// change this variable appropriately depending on which environment you're running this script in
+const baseUrl = "http://localhost:3001";
+
+function callApi(api, param, cb) {
+    param = encodeURIComponent(param.trim());
+    fetch(`${baseUrl}/api/${api}/${param}`)
+        .then(response => response.json())
+        .then(data => {
+            return cb(data);
+        });
+}
+
+const marvelApi = {
+    // Search for characters by name
+    getCharacters: function (name, cb) {
+        callApi("characters", name, cb);
+    },
+
+    // Search for comics by title
+    getComics: function(title, cb) {
+        callApi("comics", title, cb);
+    }
+};
+
+export default marvelApi;
