fcafe8aab0f9a5dbccde89bfcae58c6fe27b128b
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-04-10 15:38:45 -0700

    user signup works.  Redirect after signup to home page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const LocalStrategy = require("passport-local").Strategy;
 
 // Configure Express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware 
 app.use(express.urlencoded({ extended: true }));
