3eaf965f5d10670f7e55335539cab3d4dd2ac9b2
Liz <equilinquin@gmail.com>
2020-04-11 14:48:04 -0700

    added token

--- client/src/utils/login-api.js
+++ client/src/utils/login-api.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
 export default {
+  isLoggedIn : function(setLoading) {
+    axios.get('/api/checkToken')
+      .then(res => {
+        if (res.status !== 200) {
+          this.logout();
+        }
+        setLoading(true);
+      })
+      .catch(err => {
+        this.logout();
+      });		
+},
+logout: function() {
+  axios
+    .get("/api/logout")
+    .then(res => {
+      window.location = '/';
+    })
+    .catch(err => {
+      console.log(err);
+    })
+},
+
   login: function(data) {
     return axios.post("/user/login", data);
   },
