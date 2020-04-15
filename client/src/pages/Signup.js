2638ec314b649ca89229fe33109e6c853f68c515
jaredbucko <jaredbucko@icloud.com>
2020-04-14 17:50:17 -0700

    added logo to signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -16,9 +16,15 @@ function Signup() {
   user? <Redirect to='/home'/> :
     <div className="container" style={{paddingTop: "20px"}}>
 
-      <div className="row " style={{paddingTop: "30px"}}>
+      <div className="row" style={{marginBottom: "0"}}>
+        <div className="col s12">
+          <h1 className="title center-align" id="title" style={{marginTop: "30px", marginBottom: "20px"}}>Excelsior Comics</h1>
+        </div>
+      </div>
+
+      <div className="row">
         <div className="col s12 m6">
-          <h3>Create a New Account</h3>
+          <h3 className="center-align" style={{color: "white"}}>Create a New Account</h3>
           <p>Excelsior Comics is your one stop shop for all things Marvel comics. Sign up for a free account to save your discoveries to your reading list and keep track of your favorite titles and characters.</p>
           <p className="">Already have an account? Log in <Link to="/login">here</Link></p>
           <p className="">Click <Link to="/home">here</Link> to explore as a guest</p>
